06a0b4c1573945f501301afa23b9990c13a736c5
gitfaizankhan <faizankcs099@gmail.com>
2022-10-19 12:08:30 +0530

    switch statment convert

--- src/PrefixToInfix.java
+++ src/PrefixToInfix.java
@@ -6,32 +6,27 @@ public class PrefixToInfix {
         String result = convert(str);
         System.out.println(result);
     }
-    static String convert(String str){
-        int length = str.length();
+    static String convert(String exp){
         Stack<String> stack = new Stack<>();
-
+        int length = exp.length();
         for(int i = length-1; i >= 0; i--){
-            char c = str.charAt(i);
-            if(isOperator(c)){
-                String opt1 = stack.pop();
-                String opt2 = stack.pop();
-                String temp = "("+opt1+c+opt2+")";
-                stack.push(temp);
-            }else{
-                stack.push(c+"");
+            char character = exp.charAt(i);
+            if(isOperator(character)){
+                String s1 = stack.pop();
+                String s2 = stack.pop();
+
+                String temp_string = "(" + s1 + character + s2 +")";
+                stack.push(temp_string);
+            }else {
+                stack.push(character+"");
             }
         }
-
         return stack.pop();
     }
     static boolean isOperator(char c){
-        switch (c){
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                return true;
-        }
-        return false;
+        return switch (c) {
+            case '+', '-', '*', '/', '%', '^' -> true;
+            default -> false;
+        };
     }
 }
